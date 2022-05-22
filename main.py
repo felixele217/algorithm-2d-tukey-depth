c8b78688a199da8322d13ae94f751e2e602feae3
felixele217 <felix.droese@gmx.net>
2022-05-22 13:36:45 +0200

    bss

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from point import Point
 
 
 def main(): 
-    print("Hello from main...")
-    Point.create_sample_points()
+    # 1 create sample points
+    sample_points = Point.create_random_sample_points()
+    # 2 store sample points in quadrants
+    quadrants_with_points = Point.divide_points_into_quadrants(sample_points)
+    # 3 calculate depth i for each quadrant and return minimal depth
+    depth = Point.calculate_depth_i(quadrants_with_points)
 
 
 if __name__ == "__main__":
--- point.py
+++ point.py
@@ -1,5 +1,6 @@
 from math import sqrt
-import random
+from random import randint
+from heapq import heapify
 
 
 class Point:
@@ -7,29 +8,52 @@ class Point:
         self.x = x_init
         self.y = y_init
 
-    def shift(self, x, y):
-        self.x += x
-        self.y += y
-
     def __repr__(self):
         return "".join(["Point(", str(self.x), ",", str(self.y), ")"])
 
     @staticmethod
-    def distance(a, b):
-        return sqrt((a.x-b.x)**2+(a.y-b.y)**2)
-
-    @staticmethod
-    def create_sample_points():
+    def create_random_sample_points():
         amount_of_points = int(input("How many points do you want to create? "))
         sample_points = []
         for i in range(amount_of_points):
-            Point.create_random_sample_point()
+            sample_point = Point.create_random_sample_point()
+            sample_points.append(sample_point)
+        return sample_points
 
     @staticmethod
     def create_random_sample_point():
+        x = randint(-10, 10)
+        y = randint(-10, 10)
+        while x == 0: x = randint(-10, 10)
+        while y == 0: y = randint(-10, 10)
+        return Point(x, y)
+    
+    @staticmethod
+    def divide_points_into_quadrants(sample_points):
+        quadrant_0, quadrant_1, quadrant_2, quadrant_3 = ([] for i in range(4))
+        for sample_point in sample_points:
+            if sample_point.x > 0 and sample_point.y > 0: 
+                quadrant_0.append(sample_point)
+            elif sample_point.x < 0 and sample_point.y > 0:
+                quadrant_1.append(sample_point)
+            elif sample_point.x < 0 and sample_point.y < 0:
+                quadrant_2.append(sample_point)
+            else:
+                quadrant_3.append(sample_point)
+        return [quadrant_0, quadrant_1, quadrant_2, quadrant_3]
 
+    @staticmethod
+    def calculate_depth_i(quadrants_with_points):
+        for i in quadrants_with_points:
+            heaps_for_quadrant_i = Point.build_heaps_for_quadrant_i(i, quadrants_with_points)
 
+    @staticmethod
+    def build_heaps_for_quadrant_i(i, quadrants_with_points):
+        h_i_minus_one = heapify(quadrants_with_points[i-1 % 4])
+        h_i_plus_one = heapify(quadrants_with_points[i+1 % 4])
+        return [h_i_minus_one, h_i_plus_one]
 
-# p1 = Point(10, 3)
-# p2 = Point(1, 0)
+    # TODO ich muss depth_i für die einzelnen quadranten ausrechnen. dafür erstelle ich gerade die heaps für die jeweiligen quadranten und muss dann in
+    # TODO calculate_depth_i die einzelnen tiefen berechnen
+        
 
