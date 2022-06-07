78a51024e9ec2fd6d48f17a9aece939b0aa42458
felixele217 <felix.droese@gmx.net>
2022-06-07 15:11:24 +0200

    creating plots

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 def main(): 
-    [n, times] = run_algorithm_for_n_points(n_points=10000)
+    [n, times] = run_algorithm_for_n_points(n_points=5000)
     plot_results(n, times)
 
 
