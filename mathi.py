6fe1867649b5f43e81ef9174e58bf4c942397c1e
felixele217 <felix.droese@gmx.net>
2023-03-14 21:21:59 +0100

    mathis learning

---
+++ mathi.py
@@ -0,0 +1,34 @@
+def print_if_age_is_adult(age: int):
+    if age > 40:
+        print('This person is a boomer')
+    else:
+        print('This person is a millenial.')
+
+print_if_age_is_adult(20)
+# Output:
+# 20
+
+
+def print_numbers(numbers: list):
+    for number in numbers:
+        print(number)
+
+
+print_numbers([1, 2, 3])
+# Output:
+# 1
+# 2
+# 3
+
+# Schreibe eine Funktion, die eine Liste von 3 Zahlen entgegennimmt und ausgibt, ob die Zahl größer als 10 ist
+
+def print_if_numbers_are_larger10(numbers: list):
+    for number in numbers:
+        if number > 11:
+            print(str(number) + ' is larger than 10')
+        else:
+            print(str(number) + ' is not larger than 10')
+
+
+
+print_if_numbers_are_larger10([10, 2, 3])
